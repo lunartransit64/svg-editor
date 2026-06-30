665e30daef0518355e98c085d12d41faba6dac1f
Lunar Transit <Thomas.roberts23@outlook.com>
2026-06-30 23:14:18 +1000

    moved artboard rendering to artboard.js

--- js/artboard.js
+++ js/artboard.js
@@ -5,12 +5,24 @@ const artboard = document.getElementById('artboard');
 const widthValue = document.getElementById('artboard-width');
 const heightValue = document.getElementById('artboard-height');
 
+function updateArtboardRender() {
+  const zoom = typeof currentZoom !== 'undefined' ? currentZoom : 1;
+  // Update Artboard Render
+  const awidth = Number(artboard.getAttribute('width'));
+  const aheight = Number(artboard.getAttribute('height'));
+  
+  artboard.style.width = `${awidth * zoom}px`;
+  artboard.style.height = `${aheight * zoom}px`;
+}
+  
 // Update Width
 widthValue.addEventListener('input', () => {
   artboard.setAttribute('width', widthValue.value);
+  updateArtboardRender();
 });
 
 // Update Height
 heightValue.addEventListener('input', () => {
   artboard.setAttribute('height', heightValue.value);
+  updateArtboardRender();
 });
