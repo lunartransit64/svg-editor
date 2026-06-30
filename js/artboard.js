4c8713ac66c3745c841e0efd667ff8e154bba457
Lunar Transit <Thomas.roberts23@outlook.com>
2026-06-30 21:06:42 +1000

    fixed error

--- js/artboard.js
+++ js/artboard.js
@@ -6,11 +6,11 @@ const widthValue = document.getElementById('artboard-width');
 const heightValue = document.getElementById('artboard-height');
 
 // Update Width
-width.addEventListener('input', () => {
+widthValue.addEventListener('input', () => {
   artboard.setAttribute('width', widthValue.value);
 });
 
 // Update Height
-width.addEventListener('input', () => {
+heightValue.addEventListener('input', () => {
   artboard.setAttribute('width', heightValue.value);
 });
