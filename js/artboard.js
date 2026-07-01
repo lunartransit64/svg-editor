bc405aa8a2e7e18f0a5b9cb6c3dae505c4093b94
Lunar Transit <Thomas.roberts23@outlook.com>
2026-07-01 10:56:09 +1000

    added view box to hopefully fix scaling

--- js/artboard.js
+++ js/artboard.js
@@ -7,10 +7,14 @@ const heightValue = document.getElementById('artboard-height');
 
 function updateArtboardRender() {
   const zoom = typeof currentZoom !== 'undefined' ? currentZoom : 1;
-  // Update Artboard Render
+  // Find Width and Height Values
   const awidth = Number(artboard.getAttribute('width'));
   const aheight = Number(artboard.getAttribute('height'));
+
+  // Change/Update View Box
+  artboard.setAttribute('viewBox', `0 0 ${awidth} ${aheight}`);
   
+  // Update Display Size
   artboard.style.width = `${awidth * zoom}px`;
   artboard.style.height = `${aheight * zoom}px`;
 }
