7d76941ae734532940238950343ed6c74fd13ef2
Lunar Transit <Thomas.roberts23@outlook.com>
2026-07-01 15:12:55 +1000

    fixed up code hopefully

--- js/menubar.js
+++ js/menubar.js
@@ -1,14 +1,23 @@
 document.querySelectorAll('.menu-btn').forEach(button => {
   button.addEventListener('click', (e) => {
-    // Reset
-    document.querySelectorAll('tab-btn').forEach(button => btn.classList.remove('active'));
-    document.querySelectorAll('navbar-tab').forEach(button => btn.classList.remove('active'));
+    // Reset Menubar
+    document.querySelectorAll('menubar-btn').forEach(btn => {
+      btn.classList.remove('active');
+    });
 
+    // Reset Navbar
+    document.querySelectorAll('menubar-btn').forEach(btn => {
+      btn.classList.remove('active');
+    });
+    
     // Assign Current Clicked item to make it active
     button.classList.add('active');
 
     // Update Navbar
     const targetId = button.dataset.target;
-    document.getElementById(targetId).classList.add('active');
+    const targetPanel = document.getElementById(targetId);
+    if (targetPanel) {
+      targetPanel.classList.add('active');
+    }
   });
 });
